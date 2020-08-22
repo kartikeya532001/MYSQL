d248d4f2433630985c14f9d553566790b033b327
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2020-08-23 02:23:16 +0530

    INSERTING TO TABLE CLIENT1

---
+++ Experiment 1/INSERTING TO TABLE CLIENT1.sql
@@ -0,0 +1,9 @@
+use lab_work;
+INSERT INTO CLIENT1 
+VALUES('C00001', 'Ivan bayross','ANDHERI','EAST', 'Mumbai', 400054, 'Maharashtra', 15000),
+('C00002', 'Mamta muzumdar', 'Shanthi Colony Main Road','AI Block','Madras', 780001, 'Tamil nadu', 0),
+('C00003', 'Chhaya bankar','ANDHERI','WEST', 'Mumbai', 400057 ,'Maharashtra' ,5000),
+('C00004', 'Ashwini joshi', 'Sarvepalli Radhakrishnan Rd','Soladevanahalli', 'Bangalore', 560001, 'Karnataka', 0),
+('C00005', 'Hansel colaco', 'Goregaon', 'WEST', 'Mumbai', 400060, 'Maharashtra', 2000),
+('C00006', 'Deepak sharma','Sturrock Rd','Attavar', 'Mangalore', 560050 ,'Karnataka', 0);
+
