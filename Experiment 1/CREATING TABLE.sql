a6feb3dc018c074c0b30056373ee3aa7f40f26f5
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2020-08-22 21:16:05 +0530

    Command for creating the tables

---
+++ Experiment 1/CREATING TABLE.sql
@@ -0,0 +1,35 @@
+use lab_work;
+CREATE TABLE CLIENT1 (
+  Colunmn_name varchar(10),
+   Name1 varchar(50),
+   ADDRESS1 varchar(30),
+   ADDRESS2 varchar(30),
+   CITY varchar(15),
+   PINCODE int,
+   STATE varchar(15),
+   BALDUE decimal(10,2)
+);
+CREATE TABLE PRODUCT_Master (
+  PRODUCTNO varchar(10),
+   DESCRIPTION1 varchar(50),
+   PROFITPERCENT decimal(4,2),
+   UNIT_MEASURE varchar(30),
+   QTYONHAND int,
+   REORDERL_VL int,
+   SELLPRICE  decimaL (8,2),
+   COSTPRICE decimal(10,2)
+);
+CREATE TABLE SALESMAN_Master (
+  SALESMANNO varchar(10),
+   SALESMANName varchar(50),
+   ADDRESS1 varchar(30),
+   ADDRESS2 varchar(30),
+   CITY varchar(15),
+   PINCODE int,
+   STATE varchar(15),
+   SALAMT REAL,
+   TGTTOGET decimal(10,2),
+   YTDSALES DOUBLE(6,2),
+   REMARKS varchar(60)
+   
+);
