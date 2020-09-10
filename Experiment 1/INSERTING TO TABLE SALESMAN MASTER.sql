ccd3fad189dfd7befbecce681bee333450fc6034
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2020-09-10 20:44:12 +0700

    Inserting the data in table salesman master

---
+++ Experiment 1/INSERTING TO TABLE SALESMAN MASTER.sql
@@ -0,0 +1,6 @@
+use lab_work;
+INSERT INTO SALESMAN_Master 
+VALUES( 'S00001     ' ,'Aman          ' ,'A/14                ','Worli              ','Mumbai   ',400002,'Maharashtra'),
+('S00002     ', 'Omkar         ', '65                  ' ,'Nariman            ', 'Mumbai   ', 400001, 'Maharashtra'),
+('S00003     ' ,'Raj           ' ,'P-7                 ', 'Bandra             ','Mumbai   ',400032 ,'Maharashtra'),
+('S00004    ' ,'Ashish        ' ,'A/5                 ','Juhu               ','Mumbai   ',400044 ,'Maharashtra');
