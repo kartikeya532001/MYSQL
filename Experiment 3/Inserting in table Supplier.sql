f812fba94813ffb704fdfb8a1c07c712d87d30c4
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2020-09-25 04:32:00 +0700

    Add files via upload

---
+++ Experiment 3/Inserting in table Supplier.sql
@@ -0,0 +1,10 @@
+use lab_work;
+INSERT INTO Supplier 
+VALUES ('A0001','Haseen', 'Dehradun',50000),
+('A0002','Kishore','Delhi',1500000),
+('A0003','Gaurav','Mumbai', 200000),
+('A0004','Rachit', 'Mumbai',5400000),
+('A0005', 'Ritvik', 'Chennai',800000),
+('A0006','Akshay', 'Mumbai', 7900000),
+('A0007','Ajay', 'Delhi' , 90000),
+('A0008','Pal', 'Mumbai',5500000);
