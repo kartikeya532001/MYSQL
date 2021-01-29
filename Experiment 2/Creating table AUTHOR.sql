ccca997e26b6131329a06586fd37edca7d724e07
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-30 01:03:24 +0530

    Add files via upload

---
+++ Experiment 2/Creating table AUTHOR.sql
@@ -0,0 +1,10 @@
+use lab_work;
+CREATE TABLE AUTHOR
+(
+Author_ID  varchar(5) primary key,
+Lastname  varchar(15),
+Firstname varchar(15),
+Email varchar(40),
+ City varchar(15),
+Country varchar(15)
+);
