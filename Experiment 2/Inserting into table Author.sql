d76da3193acbc73825556dc9fbb18cea9586c98b
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-31 22:11:27 +0530

    Add files via upload

---
+++ Experiment 2/Inserting into table Author.sql
@@ -0,0 +1,6 @@
+use lab_work;
+INSERT INTO AUTHOR
+VALUES ('A0001','Khan','Haseen','haseen@gmail.com','Dehradun','India'),
+('A0002','kumar','Kishore', 'kishore@gmail.com','Delhi','India'),
+('A0003','Pant', 'Gaurav', 'gaurav@gmail.com', 'Mumbai','India'),
+('A0004', 'Katariya', 'Pal', 'Pal@gmail.com', 'Goa', 'India');
