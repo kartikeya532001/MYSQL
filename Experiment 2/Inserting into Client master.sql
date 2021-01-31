13b35dd651f577cb9c449e44d5f0291c090d04db
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-31 22:10:38 +0530

    Add files via upload

---
+++ Experiment 2/Inserting into Client master.sql
@@ -0,0 +1,7 @@
+use lab_work;
+INSERT INTO CLIENT_MASTER_1
+VALUES('c00001', 'Ivan bayross','ANDHERI ','EAST', 'Mumbai', 400054, 'Maharashtra', 15000),
+('c00002', 'Mamta muzumdar', 'Shanthi Colony Main Road','AI Block','Madras', 780001, 'Tamil nadu', 0),
+('c00003', 'Chhaya bankar','ANDHERI','WEST', 'Mumbai', 400057 ,'Maharashtra' ,5000),
+('c00004', 'Ashwini joshi', 'Sarvepalli Radhakrishnan Rd','Soladevanahalli', 'Bangalore', 560001, 'Karnataka', 0);
+
