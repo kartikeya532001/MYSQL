f442dbb0d7a7aa3e4f7a4ade5f0779cc26529e96
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-31 22:13:59 +0530

    Add files via upload

---
+++ Experiment 2/Output.sql
@@ -0,0 +1,7 @@
+use lab_work;
+select * from CLIENT_MASTER_1 ;
+select * from  PRODUCT_MASTER_1;
+select * from   SALESMAN_MASTER_1;
+select * from  AUTHOR;
+select * from BOOK ;
+select * from  AUTHOR_LIST ;
