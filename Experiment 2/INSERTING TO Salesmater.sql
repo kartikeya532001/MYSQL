6e0379591fe1b6b808dcd72a84d484d35ffe4dd0
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-31 22:13:38 +0530

    Add files via upload

---
+++ Experiment 2/INSERTING TO Salesmater.sql
@@ -0,0 +1,6 @@
+use lab_work;
+INSERT INTO SALESMAN_MASTER_1
+VALUES( 's00001' ,'Aman' ,'A/14','Worli','Mumbai',400002,'Maharashtra',56.0,85.5,556.0,'Na'),
+('s00002', 'Omkar','65','Nariman', 'Mumbai', 400001, 'Maharashtra',78.0,44.8,564.0,'NA'),
+('s00003' ,'Raj' ,'P-7', 'Bandra','Mumbai',400032 ,'Maharashtra',75.0,98.0,435.5,'Na'),
+('s00004' ,'Ashish' ,'A/5','Juhu','Mumbai',400044 ,'Maharashtra',74.5,55.2,554.3,'Na');
