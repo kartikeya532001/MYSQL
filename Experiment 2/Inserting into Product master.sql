aef0da8f7bb7a532dcbef0715d82903685ffad19
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-31 22:11:05 +0530

    Add files via upload

---
+++ Experiment 2/Inserting into Product master.sql
@@ -0,0 +1,6 @@
+use lab_work;
+INSERT INTO PRODUCT_MASTER_1
+VALUES('P00001', 'T-Shirt', 5 , 200,50, 350, 250,56),
+('P0345', 'Shirts', 6, 150 ,50 ,500, 350,53),
+('P06734' ,'Cotton jeans' ,5 ,100 ,20 ,600,450,54),
+('P08865' ,'Skirts ',5 ,75 ,30 ,450 ,300,64);
